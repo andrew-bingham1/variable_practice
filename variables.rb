aeac59a92a34a28ed4e5b506e4fae1bd3073e641
Andrew Bingham <andrewgbingham@gmail.com>
2022-12-02 14:16:39 -0700

    add put statements to variables.rb

--- variables.rb
+++ variables.rb
@@ -1,8 +1,31 @@
 name = "Andrew"
 email = "andrewgbingham@gmail.com"
 age = 33
-birthdate = 031289
+birthdate = 31289
 height = 5.916
 pocket_money = 2.65
 is_hungry = false 
-is_happy = true 
+is_happy = false 
+
+puts "One year has passed and" 
+puts name
+puts "is still using his same email" 
+puts email
+puts "Since his birthday" 
+puts birthdate 
+puts "has passed he is now" 
+age = 34 
+age # 34
+puts age
+puts "His new job is great too."
+puts "He went from having" 
+puts pocket_money 
+puts "in his pocket to now having" 
+pocket_money = 100.65 
+pocket_money # 100.65
+puts pocket_money
+puts "Is he happy?"
+is_happy = true
+is_happy # true
+puts "Answer:"
+puts is_happy
